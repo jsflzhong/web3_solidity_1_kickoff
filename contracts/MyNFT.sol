d301af56f10506e4da88ef0c53d6bc51c780f127
jsflzhong <jsflzhong@163.com>
2024-10-19 00:51:57 +0800

    fix up the problem of "Counters.sol"

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -6,26 +6,28 @@ pragma solidity ^0.8.0;
 
 // 引入ERC721标准库
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+//import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyNFT is ERC721, Ownable {
     // 使用计数器为每个NFT生成唯一ID
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    // 使用 uint256 手动管理 tokenId 的递增
+    uint256 private _currentTokenId;
 
     // 铸造NFT事件，便于追踪链上的NFT铸造行为
     event NFTMinted(address recipient, uint256 tokenId, string tokenURI);
 
     // 构造函数，初始化NFT名称和符号
-    constructor() ERC721("MyNFT", "MNFT") {}
+    constructor() ERC721("MyNFT", "MNFT") {
+         _currentTokenId = 0; // 初始化 tokenId
+    }
 
     // 铸造NFT的函数
     // 只有合约所有者可以调用此函数，向指定地址铸造NFT并分配tokenURI
     function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
-        _tokenIds.increment(); // 增加tokenId计数器
+         _currentTokenId++; // 增加tokenId计数器
 
-        uint256 newItemId = _tokenIds.current(); // 获取当前tokenId
+        uint256 newItemId = _currentTokenId; // 获取当前tokenId
 
         _mint(recipient, newItemId); // 铸造NFT并将其分配给接收者地址
         _setTokenURI(newItemId, tokenURI); // 为该NFT设置tokenURI
