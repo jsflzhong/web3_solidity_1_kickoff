711c9386e85cade2051e9c9fffdc9aab041cb546
username <jsflzhong@163.com>
2024-12-01 13:46:42 +0800

    Finish function "contribute"

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -15,6 +15,7 @@ User Journey：
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Crowdfunding is Ownable{
 
@@ -32,5 +33,34 @@ contract Crowdfunding is Ownable{
     event ContributionReceived(address contributor, uint256 amount);
     event FundRaiserSuccessful(uint256 totalRaisedAmount);
     event FundRaiseFailed();
+
+    constructor(IERC20 _token, uint256 _totalRised, uint256 _deadline) Ownable(msg.sender){
+        token = _token;
+        totalRised = _totalRised;
+        deadline = _deadline;
+    }
+
+    //Users join in the contribution
+    function contribute(uint256 amount) external {
+        require(block.timestamp < deadline, "The Crowdfunding has ended");
+        require(amount > 0, "The amount needs to be greater than zero");
+        require(totalRised + amount <= goal, "Goal exceeded");
+
+        //Transfer amount to Token
+        token.transferFrom(msg.sender, address(this), amount);
+
+        //Record contributions
+        contributions[msg.sender] += amount;
+
+        //Record total amount
+        totalRised += amount;
+
+        emit ContributionReceived(msg.sender, amount);
+
+        //Check if the goal has been achieved
+        if (totalRised >= goal) {
+            emit FundRaiserSuccessful(totalRised);
+        }
+    }
 
 }
