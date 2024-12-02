4f8ea03a281d5e33b9f7eacbfe3456d5b9479edd
username <jsflzhong@163.com>
2024-12-03 00:00:35 +0800

    implement withdraw function

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -23,7 +23,7 @@ contract Crowdfunding is Ownable{
     //Target amount
     uint256 public goal;
     //Crowdfunded amount
-    uint256 public totalRised;
+    uint256 public totalRaised;
     //The deadline for crowdfunding 
     uint256 public deadline;
 
@@ -34,9 +34,9 @@ contract Crowdfunding is Ownable{
     event FundRaiserSuccessful(uint256 totalRaisedAmount);
     event FundRaiseFailed();
 
-    constructor(IERC20 _token, uint256 _totalRised, uint256 _deadline) Ownable(msg.sender){
+    constructor(IERC20 _token, uint256 _totalRaised, uint256 _deadline) Ownable(msg.sender){
         token = _token;
-        totalRised = _totalRised;
+        totalRaised = _totalRaised;
         deadline = _deadline;
     }
 
@@ -44,7 +44,7 @@ contract Crowdfunding is Ownable{
     function contribute(uint256 amount) external {
         require(block.timestamp < deadline, "The Crowdfunding has ended");
         require(amount > 0, "The amount needs to be greater than zero");
-        require(totalRised + amount <= goal, "Goal exceeded");
+        require(totalRaised + amount <= goal, "Goal exceeded");
 
         //Transfer amount to Token
         token.transferFrom(msg.sender, address(this), amount);
@@ -53,18 +53,23 @@ contract Crowdfunding is Ownable{
         contributions[msg.sender] += amount;
 
         //Record total amount
-        totalRised += amount;
+        totalRaised += amount;
 
         emit ContributionReceived(msg.sender, amount);
 
         //Check if the goal has been achieved
-        if (totalRised >= goal) {
-            emit FundRaiserSuccessful(totalRised);
+        if (totalRaised >= goal) {
+            emit FundRaiserSuccessful(totalRaised);
         }
     }
 
     // withdraw funds
     function withdraw() external onlyOwner {
+        require(block.timestamp >= deadline, "Crowdfunding not yet ended");
+        require(totalRaised >= goal, "Funding goal not reached");
+
+        // tranfer funds to owner
+        token.transfer(owner(), totalRaised);
     }
 
 }
